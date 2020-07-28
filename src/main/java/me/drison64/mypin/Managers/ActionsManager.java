b63f0a03cabf8a3e336c728c81c30ec7a9348eef
Drison64 <Drison64@seznam.cz>
2020-07-29 01:11:47 +0200

    another test lul

--- src/main/java/me/drison64/mypin/Managers/ActionsManager.java
+++ src/main/java/me/drison64/mypin/Managers/ActionsManager.java
@@ -11,8 +11,6 @@ public class ActionsManager {
     public ActionsManager(Main main) {
         this.main = main;
     }
-    //test
-    //test another test lul lul
 
     //TODO Actionsmanager (maybe this is not needed at all lul)
 
